47b2e3b916b71bf1bfbbd2218ab5265e49f5c705
melih <melih.ekici@b2metric.com>
2022-04-27 21:24:48 +0300

    User and Micropost models are added

--- micropost/api/models.py
+++ micropost/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+
+class Micropost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    text = models.CharField(max_length=300)
+    referenced = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="reference")
+    timestamp = models.DateTimeField(auto_now=True)
